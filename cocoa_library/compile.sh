ac781520e4b83e505fdfa15075d105b388ad9b73
rikki cattermole <pbdadmin@gmail.com>
2014-12-15 18:19:57 +1300

    Multi os not working for travis

--- cocoa_library/compile.sh
+++
@@ -1,7 +0,0 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	echo -e "Compiling OSX lib"
-	
-	xctool -project cocoa_library/project/dwc-osx.xcodeproj -scheme dwc-osx
-	
-	echo -e "Done compiling OSX lib"
-fi
