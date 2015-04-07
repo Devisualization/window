5f7d324b89a83ccaad669d67cf94dfc526736e81
rikki cattermole <pbdadmin@gmail.com>
2015-04-07 12:17:07 +1200

    Fixes a bug for OpenVG

--- platforms/win32/devisualization/window/context/opengl.d
+++ platforms/win32/devisualization/window/context/opengl.d
@@ -144,7 +144,7 @@ private {
                 0,                     // no accumulation buffer  
                 0, 0, 0, 0,            // accum bits ignored  
                 32,                    // 32-bit z-buffer  
-                0,                     // no stencil buffer  
+                8,                     // 8-bit stencil buffer  
                 0,                     // no auxiliary buffer  
                 PFD_MAIN_PLANE,        // main layer  
                 0,                     // reserved  
